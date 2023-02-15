70232fb77384c850150c1d4d0c0563280a6767db
GarryGao00 <57919239+GarryGao00@users.noreply.github.com>
2023-02-15 16:00:40 -0600

    generator beta code :)

---
+++ video_generator.py
@@ -0,0 +1,124 @@
+import cv2
+import themis_imager_readfile
+import multiprocessing
+import os
+import gzip
+import logging
+import datetime
+
+def decompress_pgm_files(folder_path, decompressed_folder_path):
+        logging.info('decompress start, hour = '+folder_path[-4:])
+        '''
+        folder_path: str, should be ut** folder path
+        '''
+
+        # get all compressed images absolute path in the folder, exclude hidden files and different shape files
+        file_names = os.listdir(folder_path)
+        file_names = [folder_path+'/'+f for f in file_names if 'full' in f and not f.startswith('.')]
+
+        # read the images using themis_imager_readfile - input is the list of absolute paths to compressed images
+        img, meta, problematic_files = themis_imager_readfile.read(file_names)
+        frame_num = img.shape[2]
+
+        for frame in range(frame_num):
+            temp_file_name = folder_path[-4:]+str(frame)+'.pgm'
+            temp_path = os.path.join(decompressed_folder_path, temp_file_name)
+            cv2.imwrite(temp_path, img[:, :, frame])
+        
+        # logging.info('listdir = '+str(os.listdir(folder_path)))
+
+        logging.info(folder_path[-4:]+ ' decompress done')
+
+
+def list_and_decompress_pgm_files(outer_folder_path):
+    logging.info('list_and_decompress start')
+
+    # Create 'decompressed' folder
+    decompressed_folder_path = os.path.join(outer_folder_path, 'decompressed')
+    if os.path.isdir(decompressed_folder_path):
+        logging.info('already decompressed at '+ decompressed_folder_path)
+        return decompressed_folder_path
+
+    os.makedirs(decompressed_folder_path, exist_ok=True)
+    logging.info('decompressed folder created')
+
+    # store the paths of the hour folder
+    hours = []
+
+    # Iterate over the child folders in the outer folder
+    for folder_name in os.listdir(outer_folder_path):
+        folder_path = os.path.join(outer_folder_path, folder_name)
+        # check if it is a sub folder
+        if os.path.isdir(folder_path):
+            hours.append(folder_path)
+
+    for hour in hours:
+        decompress_pgm_files(hour, decompressed_folder_path)
+
+    logging.info('list_and_decompress done')
+    return decompressed_folder_path
+
+
+def pgm_images_to_mp4(decompressed_folder_path, output_video_path):
+    logging.info('video convertion start')
+    # Initialize a list to store the paths to the pgm files
+    pgm_file_paths = []
+    for file_name in os.listdir(decompressed_folder_path):
+        if not file_name.startswith('.'):
+            pgm_file_paths.append(os.path.join(
+                decompressed_folder_path, file_name))
+
+    # Sort the list of pgm files -- needed as we decompressed using multithreading
+    pgm_file_paths.sort()
+
+    # Initialize the video writer
+    video_writer = cv2.VideoWriter(
+        output_video_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (256, 256))
+
+    # Iterate over the pgm files and write them to the video file
+    for image_path in pgm_file_paths:
+        # file_name = image_path[-36:]
+        # date_text = file_name[:8]
+        # frame_text = file_name[9:13]
+        frame_number = image_path.split('/')[-1]
+        image = cv2.imread(image_path)  # , cv2.IMREAD_GRAYSCALE
+        # cv2.putText(
+        #     image, date_text+' ' +
+        #     frame_text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+        #     0.5, (209, 80, 0, 255), 1)
+        cv2.putText(image, frame_number,(10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (209, 80, 0, 255), 1)
+        video_writer.write(image)
+
+    # logging.info(file_name + "//" + date_text + frame_text)
+    # Release the video writer
+    video_writer.release()
+    logging.info('video convertion end')
+
+if __name__ == '__main__':
+    child_folder_path = '/Volumes/Garrys_T7/rtroyer-useful-functions/image/rank/tmp/2020-01-04/ut00'
+    outer_folder_path = '/Volumes/Garrys_T7/rtroyer-useful-functions/image/rank/tmp/2020-01-04'
+
+    output_video_path = '//Volumes/Garrys_T7/rtroyer-useful-functions/image/rank/tmp/2020-01-04/video.mp4'
+
+    logging.basicConfig(filename='video_generator.log',
+                        # encoding='utf-8',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logging.info('MiniVideoGenerator test code start ' +
+                 datetime.datetime.now().strftime("%H:%M:%S"))
+
+    decompressed_folder_path = '/Volumes/Garrys_T7/themis_video_generator/decompressed'
+
+    # test_img = '/Volumes/Garrys_T7/rtroyer-useful-functions/image/rank/tmp/2020-01-04/ut00/20200104_0002_rank_themis12_full.pgm.gz'
+    # img, meta, problematic_files = themis_imager_readfile.read(test_img)
+    # print("Number of images: %d" % (img.shape[2]))
+
+    # decompress_pgm_files tested - working
+    # decompress_pgm_files(child_folder_path, decompressed_folder_path)
+
+    decompressed_folder_path = list_and_decompress_pgm_files(outer_folder_path)
+
+    # decompressed_folder_path = list_and_decompress_pgm_files(outer_folder_path)
+    pgm_images_to_mp4(decompressed_folder_path, output_video_path)
